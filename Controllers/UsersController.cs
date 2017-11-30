9429af7f4da9f18f14b5442263940cb642f0dc24
digitopoi <hubo85@gmail.com>
2017-11-29 20:49:45 -0600

    Add key

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -11,11 +11,37 @@ using Microsoft.IdentityModel.Tokens;
 using System.Security.Claims;
 using ASP_Angular_Auth.Models;
 using Microsoft.AspNetCore.Authorization;
+using ASP_Angular_Auth.Data;
 
 namespace ASP_Angular_Auth.Controllers
 {
-    public class UsersController
+    [Authorize]
+    [Route("[controller]")]
+    public class UsersController : Controller
     {
-        
+        private IUserService _userService;
+        private IMapper _mapper;
+
+        public UsersController(
+            IUserService userService,
+            IMapper mapper)
+        {
+            _userService = userService;
+            _mapper = mapper;
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody]UserResource userResource)
+        {
+            var user = _userService.Authenticate(userResource.UserName, userResource.Password);
+
+            if (user == null)
+                return Unauthorized();
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret)
+        }
     }
 }
---
+++ Models/AppSettings.cs
@@ -0,0 +1,7 @@
+namespace ASP_Angular_Auth.Models
+{
+    public class AppSettings
+    {
+        public string Secret { get; set; }
+    }
+}
