df121774600c2ff39c077e9ce92301855ab1d7f2
digitopoi <hubo85@gmail.com>
2017-11-29 21:03:46 -0600

    Finish controller

--- Controllers/UsersController.cs
+++ Controllers/UsersController.cs
@@ -21,10 +21,12 @@ namespace ASP_Angular_Auth.Controllers
     {
         private IUserService _userService;
         private IMapper _mapper;
+        private readonly AppSettings _appSettings;
 
         public UsersController(
             IUserService userService,
-            IMapper mapper)
+            IMapper mapper,
+            IOptions<AppSettings> appSettings)
         {
             _userService = userService;
             _mapper = mapper;
@@ -41,7 +43,92 @@ namespace ASP_Angular_Auth.Controllers
 
             var tokenHandler = new JwtSecurityTokenHandler();
 
-            var key = Encoding.ASCII.GetBytes(_appSettings.Secret)
+            var key = Encoding.ASCII.GetBytes(_appSettings.Secret);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, user.Id.ToString())
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new SigningCredentials (new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+            var tokenString = tokenHandler.WriteToken(token);
+
+            //  return basic user info (without password) and token to store client-side
+            return Ok(new {
+                Id = user.Id,
+                Username = user.UserName,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Token = tokenString
+            });
+        }
+
+        [AllowAnonymous]
+        [HttpPost]
+        public IActionResult Register([FromBody]UserResource userResource)
+        {
+            //  map resource to model
+            var user = _mapper.Map<User>(userResource);
+
+            try
+            {
+                //  save
+                _userService.Create(user, userResource.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                //  return error message if there was an exception
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var users = _userService.GetAll();
+            var userResources = _mapper.Map<IList<UserResource>>(users);
+            return Ok(userResources);
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            var user = _userService.GetById(id);
+            var userResource = _mapper.Map<UserResource>(user);
+            return Ok(userResource);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Update(int id, [FromBody]UserResource userResource)
+        {
+            //  map resource to model
+            var user = _mapper.Map<User>(userResource);
+            user.Id = id;
+
+            try
+            {
+                //  save
+                _userService.Update(user, userResource.Password);
+                return Ok();
+            }
+            catch (AppException ex)
+            {
+                //  return error message if there was an exception
+                return BadRequest(ex.Message);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            _userService.Delete(id);
+            return Ok();
         }
     }
 }
