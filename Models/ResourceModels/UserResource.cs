6147bc04d025c94a032786ecfa5201e9f60e2046
digitopoi <hubo85@gmail.com>
2017-11-29 18:43:33 -0600

    Add user resource

---
+++ Models/ResourceModels/UserResource.cs
@@ -0,0 +1,15 @@
+namespace ASP_Angular_Auth.Models.ResourceModels
+{
+    public class UserResource
+    {
+        public int Id { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Password { get; set; }
+    }
+}
